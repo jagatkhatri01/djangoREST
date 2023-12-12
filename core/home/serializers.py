bb78dbd90d024c8a1bcb7c57c0422d6de998c606
jagatkhatri01 <jagatkhatri383@gmail.com>
2023-12-12 19:56:37 +0545

    added validation in serializers

--- core/home/serializers.py
+++ core/home/serializers.py
@@ -1,8 +1,34 @@
 from rest_framework import serializers
 from .models import Person
 
+
+
+import re
+
 class PeopleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = '__all__'
-        # fields = ['first_name', 'last_name', 'age']
+
+    def validate(self, data):
+        age = data.get('age')
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+
+        if age is not None and age < 18:
+            raise serializers.ValidationError("Age must be greater than 18!")
+
+        if first_name is not None and len(first_name) < 3:
+            raise serializers.ValidationError("First name must be at least 3 characters long!")
+
+        if last_name is not None and len(last_name) < 3:
+            raise serializers.ValidationError("Last name must be at least 3 characters long!")
+
+        if not re.match("^[a-zA-Z0-9]*$", first_name):
+            raise serializers.ValidationError("First name cannot include special characters.")
+        
+        if not re.match("^[a-zA-Z0-9]*$", last_name):
+            raise serializers.ValidationError("Last name cannot include special characters.")
+
+        return data
+
